794c7c18a11b6eb02aa063588992aa783a43aed5
Jorge Hernandez <jorge@cbqasolutions.com>
2020-02-11 10:56:14 -0600

    add generate upload url endpoint

--- src/controllers/FilesController.js
+++ src/controllers/FilesController.js
@@ -1,5 +1,5 @@
 import AWS from 'aws-sdk';
-import { OK, INTERNAL_SERVER_ERROR } from 'http-status-codes';
+import { OK, INTERNAL_SERVER_ERROR, BAD_REQUEST } from 'http-status-codes';
 import { respond } from '../utils/response';
 import File from '../models/File';
 
@@ -19,9 +19,16 @@ export default class FilesController {
     return new FilesController(app);
   }
 
-  static createS3Client() {
+  static _createS3Client() {
     return new AWS.S3({
       region: process.env.AWS_REGION || undefined
+    });
+  }
+
+  static _handleUnknownError(res, e) {
+    console.error(e);
+    respond(res, INTERNAL_SERVER_ERROR, {
+      message: e.message
     });
   }
 
@@ -30,11 +37,16 @@ export default class FilesController {
       `${FilesController.basePath}/list`,
       this.getFilesList.bind(this)
     );
+
+    this.app.get(
+      `${FilesController.basePath}/upload`,
+      this.getNewFileUploadUrl.bind(this)
+    );
   }
 
   _listObjects(nextContinuationToken) {
     return new Promise((resolve, reject) => {
-      const client = FilesController.createS3Client();
+      const client = FilesController._createS3Client();
 
       const params = {
         Bucket: process.env.FILES_S3_BUCKET_NAME
@@ -60,10 +72,22 @@ export default class FilesController {
     });
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  _getPresignedUrl(fileName, expiration = 900) {
+    const client = FilesController._createS3Client();
+    const params = {
+      Bucket: process.env.FILES_S3_BUCKET_NAME,
+      Key: fileName,
+      ContentType: '',
+      Expires: expiration
+    };
+    return client.getSignedUrlPromise('putObject', params);
+  }
+
   /**
    * @api {GET} /api/files/list
    * @apiName Files
-   * @apiGroup ListFiles
+   * @apiGroup List all files in bucket
    *
    * @apiDescription Get a list of all files in the bucket
    *
@@ -89,10 +113,43 @@ export default class FilesController {
       const files = await this._listObjects();
       respond(res, OK, files);
     } catch (e) {
-      console.error(e);
-      respond(res, INTERNAL_SERVER_ERROR, {
-        message: e.message
-      });
+      FilesController._handleUnknownError(res, e);
+    }
+  }
+
+  /**
+   * @api {GET} /api/files/upload
+   * @apiName Files
+   * @apiGroup Get presigned upload url
+   *
+   * @apiDescription Get a presigned S3 url for a new upload
+   *
+   * @apiParam  {String}  id  Mandatory File name of the new upload
+   *
+   * @apiSuccess  (200) {String}  url S3 presigned upload url
+   *
+   * @apiSuccessExample {json}  Success-Response
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "url": "https://jorgehdzg-node-js-express-file-sharing.s3.us-west-2.amazonaws.com/test-file.txt?AWSAccessKeyId=AKIATUGOOFPM4STDN24X&Content-Type=&Expires=1581439981&Signature=bW9YJNxOa6mZ%2FDRd96olZIOX5RY%3D"
+   *  }
+   */
+  async getNewFileUploadUrl(req, res) {
+    try {
+      const { fileName } = req.query;
+
+      if (!fileName) {
+        respond(res, BAD_REQUEST, {
+          message: '"fileName" query parameter was missing in the request.'
+        });
+        return;
+      }
+
+      const url = await this._getPresignedUrl(fileName);
+
+      respond(res, OK, { url });
+    } catch (e) {
+      FilesController._handleUnknownError(res, e);
     }
   }
 }
