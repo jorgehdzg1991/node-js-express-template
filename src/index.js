ce2c61c9e467f4332ec04c54600e01d851d0abb0
Jorge Hernandez <jorge@cbqasolutions.com>
2020-02-27 15:10:47 -0600

    switch to npm for builds

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import app from './app';
 
 app.listen(5000, () => {
+  // GET / --> sanity endpoint
+  app.get('/', (req, res) => {
+    res.send('Works!');
+    res.end();
+  });
+
   console.log('Listening on port 5000!');
 });
